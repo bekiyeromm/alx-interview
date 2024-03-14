bed0dac5e6012ba338d18977dd0a235e20850015
berekettena <tena21bereket@gmail.com>
2024-03-14 11:51:28 +0300

    requirement fixed Documentation fixed making the file executable

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,19 +1,27 @@
+#!/usr/bin/python3
+"""
+a method that determines if all the boxes can be opened.
+"""
+
+
 def canUnlockAll(boxes):
-    # Initialize a set to keep track of visited boxes
+    """Initialize a set to keep track of visited boxes
+        boxes: is a list of lists
+        """
     visited = set()
-    # Define a function to perform depth-first search
+    """Define a function to perform depth-first search"""
 
     def dfs(box):
-        # Mark the current box as visited
+        """Mark the current box as visited"""
         visited.add(box)
 
-        # Traverse through the keys in the current box
+        """Traverse through the keys in the current box"""
         for key in boxes[box]:
-            # If key correspond to box hasn't ben visited,recursively call dfs
+            """If key correspond to box hasn't ben visited,recursiv call dfs"""
             if key not in visited and key < len(boxes):
                 dfs(key)
 
-    # Start DFS from the first box (boxes[0])
+    """Start DFS from the first box (boxes[0])"""
     dfs(0)
-    # Check if all boxes have been visited
+    """Check if all boxes have been visited"""
     return len(visited) == len(boxes)
